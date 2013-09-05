64fb988c0e37d64fb3e241dc95f156072e43bf2a
varac <varacanero@zeromail.org>
2013-09-05 18:29:26 +0200

    unattended-upgrades package cannot be installed (Bug #3098)

--- manifests/unattended_upgrades.pp
+++ manifests/unattended_upgrades.pp
@@ -1,15 +1,17 @@
 class apt::unattended_upgrades {
 
   package { 'unattended-upgrades':
-    ensure  => present
+    ensure  => present,
+    require => Exec['refresh_apt']
   }
 
   apt_conf { '50unattended-upgrades':
-    source  => [
-                "puppet:///modules/site_apt/${::lsbdistid}/50unattended-upgrades.${::lsbdistcodename}",
-                "puppet:///modules/site_apt/${::lsbdistid}/50unattended-upgrades",
-                "puppet:///modules/apt/${::lsbdistid}/50unattended-upgrades.${::lsbdistcodename}",
-                "puppet:///modules/apt/${::lsbdistid}/50unattended-upgrades" ],
-    require => Package['unattended-upgrades'],
+    source      => [
+      "puppet:///modules/site_apt/${::lsbdistid}/50unattended-upgrades.${::lsbdistcodename}",
+      "puppet:///modules/site_apt/${::lsbdistid}/50unattended-upgrades",
+      "puppet:///modules/apt/${::lsbdistid}/50unattended-upgrades.${::lsbdistcodename}",
+      "puppet:///modules/apt/${::lsbdistid}/50unattended-upgrades" ],
+    require     => Package['unattended-upgrades'],
+    refresh_apt => false
   }
 }

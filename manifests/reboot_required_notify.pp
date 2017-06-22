66f1850e15e1843ba06e8fcc7245eaabe146a6ac
Louis-Philippe Véronneau <pollito@riseup.net>
2017-06-22 18:15:38 -0400

    puppet 4 compat: mark integer as string

--- manifests/reboot_required_notify.pp
+++ manifests/reboot_required_notify.pp
@@ -1,6 +1,6 @@
 class apt::reboot_required_notify {
 
-  if versioncmp($::operatingsystemmajrelease, 8) >= 0 {
+  if versioncmp($::operatingsystemmajrelease, '8') >= 0 {
     class { 'apt::reboot_required_notify::jessie': }
     # Clean up systems that were upgraded from Wheezy or earlier:
     class { 'apt::reboot_required_notify::wheezy': ensure => absent }
